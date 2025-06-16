fee32d37a1ab2baaa5e373d07b9544470093b299
Freddy1e100 <freddy1e100@gmail.com>
2025-06-16 11:26:34 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Устанавливаем порт, который требует Render
 export PORT=${PORT:-8501}
-
-# Запускаем Streamlit с привязкой к внешнему порту
 streamlit run main.py --server.port=$PORT --server.enableCORS=false
